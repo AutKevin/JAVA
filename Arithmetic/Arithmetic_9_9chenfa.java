ac3b48ad08feb2524865e46dfc06fefb7bc367df
autumn <AutKevin@users.noreply.github.com>
2017-11-10 13:10:05 +0800

    九九乘法表

---
+++ Arithmetic/Arithmetic_9_9chenfa.java
@@ -0,0 +1,11 @@
+﻿public class Arithmetic_9_9chenfa{
+    //题目：输出9*9口诀。   
+    public static void main(String[] args){
+    	for (int i = 1; i < 10; i++) {
+			for (int j = 1; j <= i; j++) {
+				System.out.print(i+"*"+j+"="+i*j+"\t");
+			}
+			System.out.println();
+		}
+    }
+}
